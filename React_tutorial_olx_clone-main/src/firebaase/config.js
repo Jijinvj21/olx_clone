4384445dcf774a9fe0fa054139ebe4ddfb59360b
Jijin vj <98830107+Jijinvj21@users.noreply.github.com>
2023-02-15 15:51:11 +0530

    Update config.js

--- React_tutorial_olx_clone-main/src/firebaase/config.js
+++ React_tutorial_olx_clone-main/src/firebaase/config.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjjwC1FJadSTWy7ZdZFrfKRQASrXkPdPI",
-  authDomain: "reactproject-1efac.firebaseapp.com",
-  projectId: "reactproject-1efac",
-  storageBucket: "reactproject-1efac.appspot.com",
-  messagingSenderId: "1068420717005",
-  appId: "1:1068420717005:web:3f5918e2dd467b11ff8c84",
-  measurementId: "G-Z33ZB8359Z"
+  API KEY
 };
 
 // Initialize Firebase
